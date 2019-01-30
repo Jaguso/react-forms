8f1411f0b730d06a93b0ff4947d322cdaa4f49ae
Jaguso <jagutzo@gmail.com>
2019-01-30 17:16:24 -0600

    some changes

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      name: "",
+      lastname: "",
+      email: "",
+      password: "",
+    }
+  }
+
+  onChangeInput = (event) => {
+    const {name, value} = event.target
+
+    console.log("Evento: ", event)
+    console.log("Valores: ", name, value)
+    this.setState({[name]: value})
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (this.validPasswords()) {
+
+    } else {
+      alert("Los passwords no coinciden")
+    }
+  }
+
   render() {
     return(
       <div className="row justify-content-center">
         <div className="col-md-10">
-          <form action="">
+          <form onSubmit={this.onSubmit}>
             <div className="row justify-content-center">
               
               <div className="col-md-10 form-group">
@@ -14,6 +42,8 @@ class Signup extends Component {
                   type="text" 
                   name="name"
                   className="form-control"
+                  vlue={this.state.name}
+                  onChange={this.onChangeInput}
                 />
               </div>
               <div className="col-md-10 form group">
@@ -22,6 +52,8 @@ class Signup extends Component {
                   type="text"
                   name="lastname"
                   className="form-control"
+                  value={this.state.lastname}
+                  onChange={this.onChangeInput}
                 />
               </div>
               <div className="col-md-10 form-group">
@@ -30,6 +62,8 @@ class Signup extends Component {
                   type="email" 
                   name="email" 
                   className="form-control" 
+                  value={this.state.email}
+                  onChange={this.onChangeInput}
                 />
               </div>
               <div className="col-md-10 form-group">
@@ -38,14 +72,8 @@ class Signup extends Component {
                   type="password" 
                   name="password" 
                   className="form-control" 
-                />
-              </div>
-              <div className="col-md-10 form-group">
-                <label htmlFor="">Confirma tu password:</label>
-                <input 
-                  type="password" 
-                  name="confirmpassword" 
-                  className="form-control" 
+                  value={this.state.password}
+                  onChange={this.onChangeInput}
                 />
               </div>
             </div>
