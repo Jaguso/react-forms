f12bbef984dfe44be460294208a16ee6004c0f80
Jaguso <jagutzo@gmail.com>
2019-01-30 16:44:51 -0600

    second commit

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -6,19 +6,56 @@ class Signup extends Component {
       <div className="row justify-content-center">
         <div className="col-md-10">
           <form action="">
-            <div className="col-md-6 form-group">
-              <label htmlFor="name">Tu nombre</label>
-              <input 
-                type="text" 
-                name="name"
-                className="form-control"
-              />
+            <div className="row justify-content-center">
+              
+              <div className="col-md-10 form-group">
+                <label htmlFor="name">Tu nombre</label>
+                <input 
+                  type="text" 
+                  name="name"
+                  className="form-control"
+                />
+              </div>
+              <div className="col-md-10 form group">
+                <label htmlFor="lastname">Tus Apellidos:</label>
+                <input 
+                  type="text"
+                  name="lastname"
+                  className="form-control"
+                />
+              </div>
+              <div className="col-md-10 form-group">
+                <label htmlFor="">Tu email:</label>
+                <input 
+                  type="email" 
+                  name="email" 
+                  className="form-control" 
+                />
+              </div>
+              <div className="col-md-10 form-group">
+                <label htmlFor="">Tu Password:</label>
+                <input 
+                  type="password" 
+                  name="password" 
+                  className="form-control" 
+                />
+              </div>
+              <div className="col-md-10 form-group">
+                <label htmlFor="">Confirma tu password:</label>
+                <input 
+                  type="password" 
+                  name="confirmpassword" 
+                  className="form-control" 
+                />
+              </div>
             </div>
-            
+            <button type="submit" className="btn btn-success" >Registrar</button>
           </form>
+
         </div>
+
       </div>
-    )
+    );
   }
 }
 
